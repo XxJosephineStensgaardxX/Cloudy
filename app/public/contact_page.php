0b3a3fab62655ffa54546acae514e1a13b58bbe9
Sofroniebruh <sofronie.albu1@student.nhlstenden.com>
2023-10-09 22:42:56 +0200

    catalogue and contact pages are finished

--- app/public/contact_page.php
+++ app/public/contact_page.php
@@ -1,48 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php require_once 'templates.php' ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Page</title>
+    <link rel="stylesheet" href="style/style.css">
     <link rel="stylesheet" href="style/style_contact_page.css">
+
 </head>
+<?php
+    footer();
+?>
 <body>
     <div>
         <div class="content">
             <h1>CONTACT US</h1>
-            <form action="index.php">
+            <form action="submit.php" method="POST">
                 <label for="Fname"></label>
-                <input type="text" id="Fname" placeholder="Insert name">
+                <input type="text" id="Fname" placeholder="INSERT NAME" name="Fname">
                 <br>
 
                 <label for="Lname"></label>
-                <input type="text" id="Lname" placeholder="Insert surname">
+                <input type="text" id="Lname" placeholder="INSERT SURNAME" name="Lname">
                 <br>
 
                 <label for="street"></label>
-                <input type="text" id="street" placeholder="Street name">
+                <input type="text" id="street" placeholder="STREET NAME" name="street">
 
                 <label for="postcode"></label>
-                <input type="text" id="postcode" placeholder="Postal code">
+                <input type="text" id="postcode" placeholder="Postal code" name="postcode">
                 <br>
                 <label for="email"></label>
-                <input type="email" id="email" placeholder="E-MAIL">
+                <input type="email" id="email" placeholder="E-MAIL" name="email">
                 <br>
 
                 <label for="phone"></label>
-                <input type="tel" id="phone" placeholder="Phone-number">
+                <input type="tel" id="phone" placeholder="Phone-number" name="phone">
 
                 <label for="country"></label>
-                <input type="text" id="country" placeholder="Country">
+                <input type="text" id="country" placeholder="Country" name="country">
                 <br>
 
                 <label for="comment"></label>
-                <textarea name="comment" id="" cols="40" rows="15" placeholder="Insert text"></textarea>
+                <textarea name="comment" id="comment" cols="40" rows="15" placeholder="Insert text" name="comment"></textarea>
                 <br>
 
-                <input type="submit">
+                <input type="submit" id="submit">
             </form>
         </div>
     </div>
+    <?php
+    footer();
+    ?>
 </body>
 </html>
---
+++ app/public/submit.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require_once 'templates.php' ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/submit.css">
+</head>
+<body style="height: 100vh;">
+    <?php
+    footer();
+    ?>
+    <main style="height: 75%;">
+        <div class="success">
+            <h1>Your message is successfully send</h1>
+        </div>
+    </main>
+    <?php
+    footer();
+    ?>
+</body>
+</html>
