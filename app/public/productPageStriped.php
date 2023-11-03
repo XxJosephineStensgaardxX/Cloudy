84b717a1a4b689841666ff413ead33f1a098cd44
XxJosephineStensgaardxX <josephine.stensgaard@student.nhlstenden.com>
2023-11-03 15:36:51 +0100

    Cleaning up in the code

--- app/public/productPageStriped.php
+++ app/public/productPageStriped.php
@@ -81,7 +81,9 @@ $lang = init();
         <form id="form" method="post" action="checkoutPage.php">
             <div class="maincontainer-flex container">
                 <div class="container-layout">
-                    <img class="chosenPicture" src="./img/socksPhotos/Sunny_socks_<?php echo isset($_GET['color']) ? $_GET['color'] : 'blue' ?>.jpg" alt="Selected Image">
+                    <img class="chosenPicture"
+                        src="./img/socksPhotos/Sunny_socks_<?php echo isset($_GET['color']) ? $_GET['color'] : 'blue' ?>.jpg"
+                        alt="Selected Image">
                     <div class="othersocks-flex">
                         <?php
 
@@ -96,7 +98,7 @@ $lang = init();
                             }
 
                             echo
-                            "
+                                "
                             <input type='radio' name='selected_image' value='$image' id='$image'  onclick=\"changeImageProductPage(this, '" . (array_values($images)[$i]) . "')\">
                             <label for='$image' style='display: $display'>
                                 <img class='othersock-item' src='$image' alt='Classic sock'>
@@ -186,7 +188,8 @@ $lang = init();
                         </p>
                     </div>
                     <div class="border-container">
-                        <input class="button" id="cart-button" type="submit" value="<?php echo $language["Put in cart"][$lang] ?>"></input>
+                        <input class="button" id="cart-button" type="submit"
+                            value="<?php echo $language["Put in cart"][$lang] ?>"></input>
                     </div>
                 </div>
             </div>
